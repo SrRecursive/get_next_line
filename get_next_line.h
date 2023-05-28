687f77fce7a6024f07dccd3db952c406be417bc5
SrRecursive <rojohnibana@gmail.com>
2023-05-28 04:17:33 +0200

    Files added

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ribana-b <ribana-b@42student.malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/27 05:01:01 by ribana-b          #+#    #+#             */
+/*   Updated: 2023/05/28 03:30:02 by ribana-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 64
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+#endif
